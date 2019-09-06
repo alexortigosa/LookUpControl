25eb933164973868e480f577b6ecab4d8dc25a3f
Alexandre Ortigosa Barragan <alexandre.ortigosa@scmspain.com>
2019-09-06 04:17:45 +0200

    console log added

--- LookUpControl/components/CustomDropDown.tsx
+++ LookUpControl/components/CustomDropDown.tsx
@@ -45,7 +45,7 @@ export default class CustomDropDown extends React.Component<Props> {
     
       private _onFilterChanged = (filterText: string): Promise<ITag[]> => {
         const {fetchXML} = this.props;
-        return retriveRecords(fetchXML,filterText).then((data) => data.map((item: ITag) => ({ key: item, name: item })));
+        return retriveRecords(fetchXML,filterText).then((data) => data.map((item: ITag) => ({ key: item, name: item }),(error:any) => console.log(error)));
       };
     
 }
