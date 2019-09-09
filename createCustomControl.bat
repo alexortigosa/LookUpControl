75fb75f3c4f6255f9d6321e8f472e16d64e38927
Alexandre Ortigosa Barragan <alexandre.ortigosa@scmspain.com>
2019-09-09 14:55:09 +0200

    bash edited

--- createCustomControl.bat
+++ createCustomControl.bat
@@ -1,11 +1,14 @@
 SETLOCAL
 @echo off
+SET PUBLISHER_NAME=scm
+SET PUBLISHER_PREFIX=scm
 SET buildCutomControlFolderName=buildSolution
 SET outFolderName=out
 SET objFolderName=obj
 SET buildCutomControlFolderPath=%CD%\%buildCutomControlFolderName%
 SET outFolderPath=%CD%\%outFolderName%
 SET objFolderPath=%CD%\%objFolderName%
+SET ROOT_FOLDER=%CD%
 ECHO deleting previous build
 ECHO %buildCutomControlFolderPath%
 ECHO %outFolderPath%
@@ -17,7 +20,14 @@ PAUSE
 ECHO Previous build deleted
 ECHO Create new build folder
 mkdir %buildCutomControlFolderPath%
-
+ECHO build npm
+npm run build
+ECHO run pac commands
+cd %buildCutomControlFolderName% 
+pac solution init --publisher-name %PUBLISHER_NAME% --publisher-prefix %PUBLISHER_PREFIX%
+pac solution add-reference --path %ROOT_FOLDER%
+msbuild /t:restore
+msbuild
 PAUSE
 
 ENDLOCAL
